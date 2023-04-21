94a7be059fbcd517be9e151b41b600ca1b7b1602
sydKB <sydneykb11@icloud.com>
2023-04-20 17:27:16 -0700

    local storage fix

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
 var eventsData;
 
+$("#currentDay").text(dayjs().format("dddd, MMMM DD"));
+
+
 function setHourColors() {
     var now = dayjs();
-    for (var i = 9; i < 18; i++) {
+    for (var i = 10; i < 24; i++) {
         if (i < now.hour()) {
             $("#hour-" + i + " textarea").addClass("past");
         } else if (i == now.hour()) {
@@ -14,33 +17,59 @@ function setHourColors() {
 }
 
 function loadStoredData() {
-    const eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
-    if (!eventsData) {
-        eventsData = {
-            hour10: "",
-            hour11: "",
-            hour12: "",
-            //etc
-        };
+    for (var i = 10; i < 24; i++) {
+        var hourText = "hour" + i + "text";
+        eventsData = JSON.parse(localStorage.getItem(hourText));
+
+        $("#hour-" + i + " textarea").text(eventsData);
     }
+    
+    // if (!eventsData) {
+    //     eventsData = {
+    //         hour10: "",
+    //         hour11: "",
+    //         hour12: "",
+    //         //etc
+    //     };
+    // }
 }
 
 function handleSaveClick(event) {
+    eventsData = {
+                    hour10: "",
+                    hour11: "",
+                    hour12: "",
+                    hour13: "",
+                    hour14: "",
+                    hour15: "",
+                    hour16: "",
+                    hour17: "",
+                    hour18: "",
+                    hour19: "",
+                    hour20: "",
+                    hour21: "",
+                    hour22: "",
+                };
+
     //grabs HTML data
     var hourBlock = $(event.target).parent();
-    var value = hourBlock.children("textarea").val();
+    var text = hourBlock.children("textarea").val();
     var hour = hourBlock.attr("id").split("-")[1];
 
-    //mod data obj
-    eventsData["hour" + hour] = value;
+    //mod data obj 
+    var hourSaved = "hour" + hour;
+    eventsData.hourSaved = text;
+    console.log("i typed: " + text);
 
     //store this hour's data in local
-    localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
+    hourText = "hour" + hour + "text"
+    localStorage.setItem(hourText, JSON.stringify(eventsData.hourSaved));
+    //localStorage.setItem("textarea", text);
 }
+
+$(".saveBtn").on("click", handleSaveClick)
 
 $(function() {
     loadStoredData();
     setHourColors();
 });
-
-$(".saveBtn").on("click", )
